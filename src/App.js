84971cf65005eb8223b6adb627a06bda96efedab
Nathan Moes <nathanmoes7@gmail.com>
2023-02-22 11:39:19 -0800

    config photo from github pfp, aand some styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { Button } from "bootstrap";
+import Image from "react-bootstrap/Image";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
@@ -12,14 +13,20 @@ function App() {
         <Container>
           <Navbar.Brand href="#home">Home</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">About</Nav.Link>
+            <Nav.Link href="#about">About</Nav.Link>
             <Nav.Link href="#features">Projects</Nav.Link>
             <Nav.Link href="#pricing">Pricing</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
-      <Container>
-        <img src="../resources/NathanMoes"></img>
+      <Container id="about">
+        <Image
+          src="https://avatars.githubusercontent.com/u/91927252?v=4"
+          alt="Nathan Moes selfie"
+          className=""
+          thumbnail={true}
+          roundedCircle={true}
+        ></Image>
         <p>
           I am a PSU student with my bachlors of science in comuter science. I
           will be continuing on to my masters in the fall of 2024. I am
