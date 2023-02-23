bcc746a62a8a600fb80ebeb8e51d5a83f0313e29
Nathan Moes <nathanmoes7@gmail.com>
2023-02-23 07:58:30 -0800

    firebase hoasting setup

--- src/App.js
+++ src/App.js
@@ -16,6 +16,27 @@ import {
   Row,
   Col,
 } from "react-bootstrap";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB5ZgVH3fnbx8XZC9LEgDhe_fWTN9BXb7Q",
+  authDomain: "personalpage-65cf1.firebaseapp.com",
+  projectId: "personalpage-65cf1",
+  storageBucket: "personalpage-65cf1.appspot.com",
+  messagingSenderId: "223575020791",
+  appId: "1:223575020791:web:11122c13efe901920aaff2",
+  measurementId: "G-M4R29S861E",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 function App() {
   return (
